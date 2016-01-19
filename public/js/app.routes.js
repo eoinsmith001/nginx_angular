494946ce98f90666769d2477d7a37eac6cb94d49
esmith <eoinsmith001@gmail.com>
2016-01-19 23:38:43 +0000

    changed to use angular ui router

--- public/js/app.routes.js
+++ public/js/app.routes.js
@@ -1,20 +1,36 @@
-angular.module('routerRoutes', ['ngRoute'])
-  .config(function($routeProvider, $locationProvider) {
-    $routeProvider
-      .when('/', {
-        templateUrl:  'views/pages/home.html',
-        controller:   'homeController',
-        controllerAs: 'home'
+angular.module('routerRoutes', ['ui.router'])
+  .config(function($stateProvider, $urlRouterProvider, $locationProvider) {
+    $urlRouterProvider.otherwise("/state1");
+    $stateProvider
+      .state('home', {
+        url          : '/home',
+        templateUrl  : 'views/pages/home.html'
       })
-      .when('/about', {
-        templateUrl:  'views/pages/about.html',
-        controller:   'aboutController',
-        controllerAs: 'about'
+      .state('home.message', {
+        url          : '/message',
+        templateUrl  : 'views/pages/home.message.html',
+        controller   : 'homeController',
+        controllerAs : 'home'
       })
-      .when('/contact', {
-        templateUrl:  'views/pages/contact.html',
-        controller:   'contactController',
-        controllerAs: 'contact'
+      .state('about', {
+        url          : '/about',
+        templateUrl  : 'views/pages/about.html',
+      })
+      .state('about.message', {
+        url          : '/message',
+        templateUrl  : 'views/pages/about.message.html',
+        controller   : 'aboutController',
+        controllerAs : 'about'
+      })
+      .state('contact', {
+        url          : '/contact',
+        templateUrl  : 'views/pages/contact.html',
+      })
+      .state('contact.message', {
+        url          : '/message',
+        templateUrl  : 'views/pages/contact.message.html',
+        controller   : 'contactController',
+        controllerAs : 'contact'
       });
     $locationProvider.html5Mode(true);
   });
